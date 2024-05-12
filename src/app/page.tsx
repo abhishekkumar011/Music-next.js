0d46daa3f3a3b2a97bacfd192a1c1e2eda56f92d
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-12 09:33:50 +0530

    style : design hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-black/[0.96] antialiased">
-      
+      <HeroSection/>
     </div>
   );
 }
