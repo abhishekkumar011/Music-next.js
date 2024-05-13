ec8cb265eddc97216e3b820a0fda5c780df2ab05
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-13 08:04:15 +0530

    style: design the Featured courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black/[0.96] antialiased">
-      <HeroSection/>
+      <HeroSection />
+      <FeaturedCourses />
     </div>
   );
 }
