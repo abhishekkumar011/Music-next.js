efc235f84530b487cb759e1976f08a94b4818bf6
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-05-13 14:50:04 +0530

    style: design upcoming webinar section

--- src/components/TestimonialsCards.tsx
+++ src/components/TestimonialsCards.tsx
@@ -37,7 +37,7 @@ const musicSchoolTestimonials = [
 const MusicSchoolTestimonials = () => {
   return (
     <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-      <h2 className="text-3xl font-bold text-center my-10 z-10">Hear our Harmony: Voices of success</h2>
+      <h2 className="text-3xl font-bold text-center mb-10 z-10">Hear our Harmony: Voices of success</h2>
       <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
         <div className="w-full">
           <InfiniteMovingCards
